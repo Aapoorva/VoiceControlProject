a1af700b9a70d1f4b6c6eb95ebf6beec8f42c582
abhishekkumbhaj01289 <kumbhaj.abhishek1289@gmail.com>
2018-05-27 13:41:29 +0530

    checked input loop

--- talkNwork.py
+++ talkNwork.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 import SpeechRego as sr
 import tts
-import os
+import subprocess
 
-#intro message
+# intro message
 tts.convert_text_n_speak("Welcome What You Want to be done")
-#getting text of user audio
-user_query = sr.get_audio_to_text()
-
-print(user_query)
-#cheching query is empty or not
-if user_query != None :
-	output = ""
-	try:
-		output = os.system(user_query)
-	except TypeError:
-		print("Getting None as user query")
-
-	print(output)
+# getting text of user audio
+while True:
+	user_query = sr.get_audio_to_text()
+	print(user_query)
+    # program exit keywords
+	exit_keyword = ['quit','exit','cancel','close']
+    # cheching query is empty or not
+	if user_query != None :
+		# if query has exit keywords, close the application
+		if user_query in exit_keyword:
+			tts.convert_text_n_speak('exiting')
+			break;
+		# else execute the command
+		else:
+			output = ""
+			try:
+				output = subprocess.run([user_query])
+			except TypeError:
+				print("Getting None as user query")
+			print(output)
+	tts.convert_text_n_speak('What else do you want ?')
