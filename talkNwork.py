8c2391feaa9045fb77df724aee1317ef40fffd5b
Aapoorva <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-27 13:24:36 +0530

    replaced os.system to subprocess

--- talkNwork.py
+++ talkNwork.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 import SpeechRego as sr
 import tts
-import os
+import subprocess as sp
 
 #intro message
-tts.convert_text_n_speak("Welcome What You Want to be done")
+tts.convert_text_n_speak("Welcome")
+tts.convert_text_n_speak("What You Want to be done")
+
 #getting text of user audio
 user_query = sr.get_audio_to_text()
 
-print(user_query)
-#cheching query is empty or not
+#checking query is empty or not
 if user_query != None :
-	output = ""
-	try:
-		output = os.system(user_query)
-	except TypeError:
-		print("Getting None as user query")
+	cmd_output = ""
+	try :
+		cmd_output = sp.run([user_query],check=True,shell=True,stdout=sp.PIPE,)
+	except  sp.CalledProcessError :
+		print("CalledProcessError")
 
-	print(output)
+	if cmd_output != None :
+
+		if cmd_output.returncode != 0 :
+			print(user_query," Not Found")
+		
+		else :
+			print(cmd_output.stdout.decode())
